e191e754be11be8287cc8dab5a40f3430dde57ed
Kenan Yildirim <kenan@kenany.me>
2020-10-29 17:39:51 -0400

    chore(deps): eslint@^7.12.1, eslint-config@^8.0.0

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
-var lander = require('../');
 var test = require('tape');
 var isFunction = require('lodash.isfunction');
 var almostEqual = require('almost-equal');
+
+var lander = require('../');
 
 test('exports a function', function(t) {
   t.plan(1);
