5a9ab86fad743279c4f2f8b06280ebb310920b28
Kenan Yildirim <kenan@kenany.me>
2021-08-03 14:10:51 -0400

    chore(deps): @kenan/eslint-config@9.0.4
    Closes https://github.com/KenanY/lander-formula/pull/51

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Calculates the one-repetition maximum according to Lander's formula.
+ *
+ * @param {number} w Weight.
+ * @param {number} r Reps.
+ * @returns {number} One-repetition maximum.
+ */
 function lander(w, r) {
   return 100 * w / (101.3 - 2.67123 * r);
 }
--- test/index.js
+++ test/index.js
@@ -1,20 +1,20 @@
-var test = require('tape');
-var isFunction = require('lodash.isfunction');
-var almostEqual = require('almost-equal');
+const test = require('tape');
+const isFunction = require('lodash.isfunction');
+const almostEqual = require('almost-equal');
 
-var lander = require('../');
+const lander = require('../');
 
-test('exports a function', function(t) {
+test('exports a function', (t) => {
   t.plan(1);
   t.ok(isFunction(lander));
 });
 
-test('100 for 10', function(t) {
+test('100 for 10', (t) => {
   t.plan(1);
   t.ok(lander(100, 10), 133);
 });
 
-test('100 for 6', function(t) {
+test('100 for 6', (t) => {
   t.plan(1);
   t.ok(almostEqual(lander(100, 6), 117, 1));
 });
